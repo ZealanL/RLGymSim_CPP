de435595d2233703e3a8bad70d63a26be114f91c
ZealanL <zlaferte@gmail.com>
2024-02-27 15:15:22 -0800

    Add discrete action parser

--- src/Utils/ActionParsers/ActionParser.h
+++ src/Utils/ActionParsers/ActionParser.h
@@ -5,9 +5,10 @@
 
 // https://github.com/AechPro/rocket-league-gym-sim/blob/main/rlgym_sim/utils/obs_builders/obs_builder.py
 namespace RLGSC {
+	// TODO: Only designed for discrete actions currently 
 	class ActionParser {
 	public:
-		typedef std::vector<int> Input;
+		typedef IList Input;
 
 		virtual ActionSet ParseActions(const Input& actionsData, const GameState& gameState) = 0;
 		virtual int GetActionAmount() = 0;
